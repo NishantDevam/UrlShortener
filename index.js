252530ac85a087a54dea7eade4477cd2e6e5fbc5
DevamNishant <nishant.devam@viralops.com>
2025-08-28 20:04:24 +0530

    url shortener implemented with static db

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+let urlDatabase=[];
+let counter=1;
+
 app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +23,32 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res){
+    const url=req.body.url;
+
+    const urlRegex = /^(http|https)(:\/\/)/;
+    if (!urlRegex.test(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+    const short_url=counter++;
+
+    urlDatabase[short_url]=url;
+
+    return res.json({original_url:url, short_url:short_url});
+});
+
+app.get('/api/shorturl/:short_url', function(req, res){
+    const short_url=req.params.short_url;
+    const original_url=urlDatabase[short_url];
+
+    if(original_url){
+        return res.redirect(original_url);
+    }else{
+        return res.json({error: 'invalid url'});
+    }
 });
 
 app.listen(port, function() {
